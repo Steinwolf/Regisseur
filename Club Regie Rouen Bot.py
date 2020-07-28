0c8e57354d47d892807c4dad088ad675104f2b44
Unknown <vincent-j@live.fr>
2020-07-28 12:45:46 +0200

    all fonction in object form
    need to find help and commend

--- Club Regie Rouen Bot.py
+++ Club Regie Rouen Bot.py
@@ -13,75 +13,36 @@ bot = commands.Bot(command_prefix='!')
 
 
 @bot.command()
-async def role(ctx, role=None, personne=None):
+async def role(ctx):
+    role_name = discord.utils.get(ctx.guild.roles, name="Bot testing")
+    await ctx.author.add_roles(role_name)
+    msg = 'The role {0} was added to {1}'.format(role_name.mention, ctx.author.mention)
+    await ctx.send(msg)
 
-    if role is None:
 
-        msg = '{0.author.mention} please state a role.'.format(ctx)
-        await ctx.send(msg)
+@bot.command()
+async def clear(ctx, value=-1):
+
+    if value == -1:
+        msg = 'You did not specify how many message you wanted to delete {0}.'.format(ctx.author.mention)
+        ctx.send(msg)
 
     else:
-        role_exist = False
+        async with ctx.channel.typing():
+            async for m in ctx.history(limit=value + 1):
+                await m.delete()
 
-        for f in ctx.guild.roles:
-            if role == f:
-                role_exist = True
+            msg = '{0} messages has been deleted {1.author.mention}'.format(value, ctx)
 
-        if role_exist is False:
-
-            msg = '{0.author.mention} the role you mentionned doesn\'t exist. Please try again'.format(ctx)
             await ctx.send(msg)
-        else:
-            if personne is None:
-                await ctx.author.add_roles(role)
-                msg = 'The role : "{0}" has been accorded to {1}'.format(role, ctx.author)
-                await ctx.send(msg)
-            else:
-                await personne.add_roles(role)
-                msg = 'The role : "{0}" has been accorded to {1}'.format(role, personne)
-                await ctx.send(msg)
 
 
+@bot.command()
+async def react(ctx, emote):
 
-    """if message.content.startswith('!help'):
-        msg = 'Hello, i am currently in development, but i will be ready sooner or later (probably later)' \
-              ' i will be back to you {0.author.mention}'.format(message)
-        await message.author.send(msg)
+    msg = await ctx.send("message")
 
-    if message.content.startswith("!role"):
-
-        role = discord.utils.get(message.guild.roles, name="Bot testing")
-        await message.channel.send(role)
-        await message.author.add_roles(role)
-
-    if message.content.startswith("!clear"):
-
-        breaking = message.content.split(" ")
-
-        if len(breaking) < 2:
-            msg = 'Can you tell me how many message must i delete {0.author.mention}'.format(message)
-            await message.channel.send(msg)
-        else:
-            value = int(breaking[1])
-            async with message.channel.typing():
-                async for m in message.channel.history(limit=value+1):
-                    await m.delete()
-
-                msg = '{0} messages has been deleted {1.author.mention}'.format(value, message)
-
-                await message.channel.send(msg)
-"""
-    """if message.content.startswith("!react"):
-
-        breaking = message.content.split(" ")
-
-        e = breaking[1]
-
-        msg = await message.channel.send("message")
-
-        await msg.add_reaction(e)
-
-        await message.channel.send(msg.reaction)"""
+    await msg.add_reaction(emote)
 
 
 @bot.event
