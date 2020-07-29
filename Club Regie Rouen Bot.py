8b24069791aad56b11dcade01e936f97256d8857
Unknown <vincent-j@live.fr>
2020-07-29 02:20:43 +0200

    Complete function
    Role upgraded, "?" arg in prog

--- Club Regie Rouen Bot.py
+++ Club Regie Rouen Bot.py
@@ -1,4 +1,4 @@
-# v0.2
+# v0.3
 import discord
 from discord.ext import commands
 
@@ -13,30 +13,39 @@ bot.remove_command('help')
 
 # Begin define bot command
 @bot.command()
-async def clear(ctx, value=None):
+async def clear(ctx, value=None, *, end=None):
 
     channel = ctx.channel
     author = ctx.author
-    message_history = ctx.history
 
-    # Define display message and value depending of argument
-    if value is None:
-        msg = 'You did not specify how many message you wanted to delete {0}.'.format(author.mention)
-        channel.send(msg)
-
-    elif value == "all":
-        msg = '{0} messages has been deleted by {1}'.format(value, author.mention)
-        value = 10000
-
+    if end is not None and end[-1] == '?':
+        msg = '!clear(value), value argument must be an positive int'
+        await author.send(msg)
+    elif value == '?':
+        msg = '!clear(value), value argument must be an positive int'
+        await author.send(msg)
     else:
-        msg = '{0} messages has been deleted by {1}'.format(value, author.mention)
-        value = int(value) + 1
 
-    # Delete messages
-    async with ctx.channel.typing():
-        async for m in message_history(limit=int(value) + 1):
-            await m.delete()
-        await channel.send(msg)
+        message_history = ctx.history
+
+        # Define display message and value depending of argument
+        if value is None:
+            msg = 'You did not specify how many message you wanted to delete {0}.'.format(author.mention)
+            channel.send(msg)
+
+        elif value == "all":
+            msg = '{0} messages has been deleted by {1}'.format(value, author.mention)
+            value = 10000
+
+        else:
+            msg = '{0} messages has been deleted by {1}'.format(value, author.mention)
+            value = int(value) + 1
+
+        # Delete messages
+        async with ctx.channel.typing():
+            async for m in message_history(limit=int(value) + 1):
+                await m.delete()
+            await channel.send(msg)
 
 
 @bot.command()
@@ -50,16 +59,29 @@ async def react(ctx, emote):
 
 
 @bot.command()
-async def role(ctx):
+async def role(ctx, role_given, person_given=None):
 
     guild = ctx.guild
     channel = ctx.channel
     author = ctx.author
-    role_name = discord.utils.get(guild.roles, name="Bot testing")
+
+    # Begin parsing argument
+    if person_given is None:
+        person_name = author
+    elif person_given[0] == '<':
+        person_name = discord.utils.get(guild.members, mention=person_given)
+    else:
+        person_name = discord.utils.get(guild.members, name=person_given)
+
+    if role_given[0] == '<':
+        role_name = discord.utils.get(guild.roles, mention=role_given)
+    else:
+        role_name = discord.utils.get(guild.roles, name=role_given)
+    # End parsing argument
 
     # Give specific role to user / Need upgrade
-    await author.add_roles(role_name)
-    msg = 'The role {0} was added to {1}'.format(role_name.mention, author.mention)
+    await person_name.add_roles(role_name)
+    msg = 'The role {0} was added to {1}'.format(role_name.mention, person_name.mention)
     await channel.send(msg)
 
 
@@ -102,12 +124,14 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx, error):
 
+    channel = ctx.channel
     author = ctx.author
-    msg = 'You have misstype the command or the argument {0}\n' \
-          'if the problem persist, contact an admin with the error code {1}'.format(error, author)
+    msg = 'You have misstype the command or the argument ""{0}"", try to use "!help"\n' \
+          'if the problem persist, contact an admin with the error code {1}'.format(error, author.mention)
 
     # Send error message to user
-    await ctx.send(msg)
+    await channel.send('error')
+    await author.send(msg)
 
 
 # End define bot event
